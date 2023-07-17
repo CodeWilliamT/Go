b80663c6087a94d8662e662332c18f0cc965d319
William <624573425@qq.com>
2023-07-17 10:26:00 +0000

    v0.1 finished

--- golang-IM-System/client.go
+++ golang-IM-System/client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 type Client struct {
@@ -30,6 +32,12 @@ func NewClient(serverIp string, serverPort int) *Client {
 	return client
 }
 
+// deal server response
+func (client *Client) DealResponse() {
+	//alway waiting for response
+	io.Copy(os.Stdout, client.conn)
+}
+
 func (client *Client) menu() bool {
 
 	modes := map[int]string{
@@ -54,6 +62,71 @@ func (client *Client) menu() bool {
 	}
 }
 
+func (client *Client) ListUsers() {
+	sendMsg := "-userlist\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn Write err: ", err)
+		return
+	}
+}
+
+func (client *Client) PrivateChat() {
+	remoteName := ""
+	chatMsg := ""
+
+	for remoteName != "exit" {
+		client.ListUsers()
+		fmt.Println(">>>>Please input target username,\"exit\" to exit")
+		fmt.Scanln(&remoteName)
+		for chatMsg != "exit" {
+			fmt.Println(">>>>Please input your message, \"exit\" to exit")
+			fmt.Scanln(&chatMsg)
+			if len(chatMsg) != 0 {
+				sendMsg := "-to|" + remoteName + "|" + chatMsg + "\n"
+				_, err := client.conn.Write([]byte(sendMsg))
+				if err != nil {
+					fmt.Println("conn Write err: ", err)
+					break
+				}
+			}
+			chatMsg = ""
+		}
+	}
+}
+
+func (client *Client) PublicChat() {
+	chatMsg := ""
+
+	for chatMsg != "exit" {
+		fmt.Println(">>>>Please input your message, \"exit\" to exit")
+		fmt.Scanln(&chatMsg)
+		if len(chatMsg) != 0 {
+			sendMsg := chatMsg + "\n"
+			_, err := client.conn.Write([]byte(sendMsg))
+			if err != nil {
+				fmt.Println("conn Write err: ", err)
+				break
+			}
+		}
+		chatMsg = ""
+	}
+
+}
+
+func (client *Client) UpdateName() bool {
+	fmt.Println(">>>>Please enter User Name:")
+	fmt.Scanln(&client.Name)
+
+	sendMsg := "-rename|" + client.Name + "\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn.Write err: ", err)
+		return false
+	}
+	return true
+}
+
 func (client *Client) Run() {
 	for client.flag != 0 {
 		for client.menu() != true {
@@ -62,12 +135,15 @@ func (client *Client) Run() {
 		switch client.flag {
 		case 1:
 			fmt.Println("Select Public-Chat")
+			client.PublicChat()
 			break
 		case 2:
 			fmt.Println("Select Private-Chat")
+			client.PrivateChat()
 			break
 		case 3:
 			fmt.Println("Select Rename")
+			client.UpdateName()
 			break
 		case 0:
 			break
@@ -92,6 +168,10 @@ func main() {
 		fmt.Println(">>>>>Failed to connect server...")
 		return
 	}
+
+	//start a thread to deal response
+	go client.DealResponse()
+
 	fmt.Println(">>>>>Succeed to connect server...")
 	client.Run()
 }
